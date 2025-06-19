48a1cc88a27418221cbd049071ab9b05d8162cad
Arjun Mitra <arjunmitra06082004@gmail.com>
2025-06-19 10:13:01 +0530

    Update TRAPIZOIDAL.c

--- TRAPIZOIDAL.c
+++ TRAPIZOIDAL.c
@@ -26,3 +26,30 @@ int main()
     printf("The value of the intergral is : %f\n", t);
     return 0;
 }
+
+// algo 
+
+Start
+
+Input a    // Lower limit
+Input b    // Upper limit
+Input n    // Number of intervals
+
+Set h = (b - a) / n
+Set sum = 0
+
+For i from 1 to n - 1 do
+    p = a + i * h
+    sum = sum + f(p)
+End For
+
+t = (h / 2) * (f(a) + f(b) + 2 * sum)
+
+Display "The value of the integral is: ", t
+
+End
+
+// Function Definition:
+Function f(x)
+    Return 1 / sqrt(1 + x^3)
+End Function
