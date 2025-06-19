8fca6c225f3bef951f7325f30ea3ff4cae27dc46
Arjun Mitra <arjunmitra06082004@gmail.com>
2025-06-19 10:20:08 +0530

    Update weedle.c

--- weedle.c
+++ weedle.c
@@ -40,3 +40,42 @@ int main() {
 
     return 0;
 }
+
+
+
+// algo 
+
+Start
+
+Define function f(x)
+    Return 1 / (x*x + 1)
+End Function
+
+Input a     // Lower limit
+Input b     // Upper limit
+
+Repeat
+    Input n    // Number of subintervals
+    If n mod 6 != 0 then
+        Print "Number of subintervals must be multiple of 6"
+    End If
+Until n mod 6 == 0
+
+Set h = (b - a) / n
+
+For i from 0 to n do
+    p[i] = a + i * h
+    fp[i] = f(p[i])
+End For
+
+Set s = 0
+
+For i from 0 to n - 6 step 6 do
+    s = s + fp[i] + 5*fp[i+1] + fp[i+2] + 6*fp[i+3] + fp[i+4] + 5*fp[i+5] + fp[i+6]
+End For
+
+Set t = (3 * h / 10) * s
+
+Print "The value of the integral is: ", t
+
+End
