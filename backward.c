cbc92e8c8a10c008304dae3be3815ee2cb5c4359
Arjun Mitra <arjunmitra06082004@gmail.com>
2025-06-19 10:13:55 +0530

    Update backward.c

--- backward.c
+++ backward.c
@@ -61,3 +61,67 @@ int main() {
 
     return 0;
 }
+
+
+// algo 
+
+Start
+
+Input n     // Number of data points
+
+Declare arrays x[n], y[n][n]
+
+// Input x values
+For i from 0 to n - 1 do
+    Input x[i]
+End For
+
+// Input y values (y[i][0])
+For i from 0 to n - 1 do
+    Input y[i][0]
+End For
+
+// Calculate uniform step size h
+Set h = x[1] - x[0]
+
+// Construct backward difference table
+For i from 1 to n - 1 do
+    For j from n - 1 down to i do
+        y[j][i] = y[j][i - 1] - y[j - 1][i - 1]
+    End For
+End For
+
+Display "Backward difference table"
+For j from 0 to n - 1 do
+    For i from 0 to j do
+        Print y[j][i]
+    End For
+    Print newline
+End For
+
+// Input the interpolation value
+Input p
+
+// Compute v = (p - x[n-1]) / h
+Set v = (p - x[n-1]) / h
+Set s = y[n-1][0]
+Set t = 1
+
+For i from 1 to n - 1 do
+    t = t * (v + (i - 1))
+    s = s + (t * y[n-1][i]) / fact(i)
+End For
+
+Display "The interpolated value at x = ", p, " is: ", s
+
+End
+
+// Function to compute factorial
+Function fact(n)
+    f = 1
+    While n > 0 do
+        f = f * n
+        n = n - 1
+    End While
+    Return f
+End Function
